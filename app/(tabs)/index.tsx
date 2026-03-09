e73787955dcd64c7020e9546ee7c4b6c56f42f45
danielapolanco <pinondaniela86@gmail.com>
2026-03-09 13:41:26 -0700

    Primer avance front , y todo esta local sin base de datos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,195 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import NavBar from "@/components/navBar";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { imageStyles } from "@/constants/imageStyles";
+import { styles } from "@/constants/StyleSheet";
+import { Image } from "expo-image";
+import { router } from "expo-router";
+import React from "react";
+import {
+  Dimensions,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+const { width } = Dimensions.get("window");
+const GAP = 16;
+// Eliminamos el PADDING_SCREEN  para que ocupen todo el ancho
+const CARD_WIDTH = (width - GAP) / 2;
+const PADDING_INTERNO_SCREEN = 20; // (El padding que suele traer el Parallax por defecto)
 
 export default function HomeScreen() {
+  const renderCard = (title: string, imageSource: any) => (
+    <TouchableOpacity
+      onPress={() =>
+        router.push({
+          pathname: "/receta",
+          params: { title },
+        })
+      }
+    >
+      <View style={[styles.card, { width: CARD_WIDTH }]}>
+        <Image source={imageSource} style={styles.cardImage} />
+        <Text style={styles.cardTitle}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#f8d6af", dark: "#d8c3a3" }}
       headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+        <View style={imageStyles.headerImageContainer}>
+          <Image
+            source={require("@/assets/images/homepastel.jpg")}
+            style={imageStyles.headerImage}
+          />
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+          <NavBar />
+        </View>
+      }
+    >
+      {/* Sección Pasteles */}
+      <View style={{ marginTop: .3 }}>
+        <Text
+          style={{
+            fontSize: 22,
+            fontWeight: "bold",
+            marginLeft: 0, // Ajustado a 0 porque el contenedor ya tiene padding
+            color: "hsl(29, 49%, 59%)",
+          }}
+        >
+          Pasteles
+        </Text>
+
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{
+            marginVertical: 10,
+            // ESTO ELIMINA LOS BORDES LATERALES
+            marginHorizontal: -PADDING_INTERNO_SCREEN,
+          }}
+          contentContainerStyle={{
+            // Añade espacio solo al inicio y final del scroll interno
+            paddingHorizontal: PADDING_INTERNO_SCREEN,
+            gap: GAP,
+          }}
+        >
+          {renderCard("Red velvet", require("@/assets/images/pastel1.jpg"))}
+          {renderCard("Chocolate", require("@/assets/images/pastel2.jpg"))}
+          {renderCard("Vainilla", require("@/assets/images/pastel3jpg.jpg"))}
+          {renderCard("Fresa Crema", require("@/assets/images/pastel4.jpg"))}
+        </ScrollView>
+        <TouchableOpacity
+          style={{
+            marginTop: .02,
+            alignSelf: "center",
+            backgroundColor: "hsl(29, 49%, 59%)",
+            paddingVertical: 10,
+            paddingHorizontal: 48,
+            borderRadius: 8,
+          }}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>
+            Ver más
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Sección Pays */}
+      <View style={{ marginTop: 25, marginBottom: 30 }}>
+        <Text
+          style={{
+            fontSize: 22,
+            fontWeight: "bold",
+            marginLeft: 0,
+            color: "hsl(29, 49%, 59%)",
+          }}
+        >
+          Pays
+        </Text>
+
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{
+            marginVertical: 10,
+            marginHorizontal: -PADDING_INTERNO_SCREEN,
+          }}
+          contentContainerStyle={{
+            paddingHorizontal: PADDING_INTERNO_SCREEN,
+            gap: GAP,
+          }}
+        >
+          {renderCard("Pay de Queso", require("@/assets/images/payQueso.jpg"))}
+          {renderCard("Pay de Limón", require("@/assets/images/payLimon.jpg"))}
+          {renderCard("Pay de Manzana", require("@/assets/images/payManzana.jpg"),)}
+          {renderCard("Pay Frutal", require("@/assets/images/payFrutal.jpg"))}
+        </ScrollView>
+
+        <TouchableOpacity
+          style={{
+            marginTop: 3,
+            alignSelf: "center",
+            backgroundColor: "hsl(29, 49%, 59%)",
+            paddingVertical: 10,
+            paddingHorizontal: 48,
+            borderRadius: 8,
+          }}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>
+            Ver más
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* Sección Galletas*/}
+      <View style={{ marginTop: 25, marginBottom: 30 }}>
+        <Text
+          style={{
+            fontSize: 22,
+            fontWeight: "bold",
+            marginLeft: 0,
+            color: "hsl(29, 49%, 59%)",
+          }}
+        >
+          Galletas
+        </Text>
+
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{
+            marginVertical: 10,
+            marginHorizontal: -PADDING_INTERNO_SCREEN,
+          }}
+          contentContainerStyle={{
+            paddingHorizontal: PADDING_INTERNO_SCREEN,
+            gap: GAP,
+          }}
+        >
+          {renderCard("Galleta de chocolate", require("@/assets/images/galletaChoco.jpg"),)}
+          {renderCard("Galleta Red velvet", require("@/assets/images/GalletaRed.jpg"),)}
+          {renderCard("Galleta Biscoff", require("@/assets/images/galletaBiscoff.jpg"),)}
+          {renderCard("Galleta Pistacho Choco", require("@/assets/images/galletaPistacho.jpg"),)}
+        </ScrollView>
+
+        <TouchableOpacity
+          style={{
+            marginTop: 3,
+            alignSelf: "center",
+            backgroundColor: "hsl(29, 49%, 59%)",
+            paddingVertical: 10,
+            paddingHorizontal: 48,
+            borderRadius: 8,
+          }}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>
+            Ver más
+          </Text>
+        </TouchableOpacity>
+      </View>
     </ParallaxScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
---
+++ app/receta.tsx
@@ -0,0 +1,139 @@
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Image } from "expo-image";
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { Alert, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring
+} from "react-native-reanimated";
+
+export default function RecetaScreen() {
+  const { title } = useLocalSearchParams();
+  const [isFavorite, setIsFavorite] = useState(false);
+  const animatedValue = useSharedValue(0);
+  // estas estan locales en la carpeta assets (estas las podemos mandar igual a la base de datos locales, para ya no mover las imagenes POLETH)
+  const images: any = {
+    "Red velvet": require("@/assets/images/pastel1.jpg"),
+    Chocolate: require("@/assets/images/pastel2.jpg"),
+    Vainilla: require("@/assets/images/pastel3jpg.jpg"),
+    "Fresa Crema": require("@/assets/images/pastel4.jpg"),
+    "Pay de Queso": require("@/assets/images/payQueso.jpg"),
+    "Pay de Limón": require("@/assets/images/payLimon.jpg"),
+    "Pay de Manzana": require("@/assets/images/payManzana.jpg"),
+    "Pay Frutal": require("@/assets/images/payFrutal.jpg"),
+    "Galleta de chocolate": require("@/assets/images/galletaChoco.jpg"),
+    "Galleta Red velvet": require("@/assets/images/GalletaRed.jpg"),
+    "Galleta Biscoff": require("@/assets/images/galletaBiscoff.jpg"),
+    "Galleta Pistacho Chocolate": require("@/assets/images/galletaPistacho.jpg"),
+  };
+
+  useEffect(() => {
+    const checkStatus = async () => {
+      try {
+        const storedFavs = await AsyncStorage.getItem("mis_favoritos");
+        if (storedFavs) {
+          const favsArray = JSON.parse(storedFavs);
+          const exists = favsArray.some((item: any) => item.title === title);
+          setIsFavorite(exists);
+          animatedValue.value = exists ? 1 : 0;
+        }
+      } catch (error) {
+        console.error("Error al cargar favoritos:", error);
+      }
+    };
+    checkStatus();
+  }, [title]);
+
+
+  const toggleFavorite = async () => {
+    try {
+      const storedFavs = await AsyncStorage.getItem("mis_favoritos");
+      let favsArray = storedFavs ? JSON.parse(storedFavs) : [];
+
+      if (isFavorite) {
+        favsArray = favsArray.filter((item: any) => item.title !== title);
+      } else {
+        favsArray.push({ title: title, image: title });
+      }
+
+      await AsyncStorage.setItem("mis_favoritos", JSON.stringify(favsArray));
+
+      const toValue = isFavorite ? 0 : 1;
+      animatedValue.value = withSpring(toValue, { damping: 15 });
+      setIsFavorite(!isFavorite);
+    } catch (error) {
+      Alert.alert("Error", "No se pudo actualizar favoritos");
+    }
+  };
+
+  const buttonAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: withSpring(isFavorite ? 1.2 : 1) }],
+  }));
+
+  const iconAnimatedStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(animatedValue.value, [0, 1], ["#555", "#ff4d4d"]);
+    return { color };
+  });
+  // son para los contenedores 
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <View style={styles.imageWrapper}>
+        <Image source={images[title as string]} style={styles.image} contentFit="cover" />
+        <Pressable onPress={toggleFavorite} style={styles.favoriteButtonContainer}>
+          <Animated.View style={[styles.favoriteButton, buttonAnimatedStyle]}>
+            <Animated.Text style={iconAnimatedStyle}>
+              <Ionicons name={isFavorite ? "heart" : "heart-outline"} size={28} color="inherit" />
+            </Animated.Text>
+          </Animated.View>
+        </Pressable>
+      </View>
+
+      <View style={styles.content}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subtitle}>Ingredientes</Text>
+        <Text style={styles.text}>• Harina, Azúcar, Huevos, Leche</Text>
+        <Text style={styles.subtitle}>Preparación</Text>
+        <Text style={styles.text}>Mezclar y hornear a 180°C por 35 min.</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+// Esta parte estoy pesnando en moverla a un archivo de estilos por mientras dejarlo aaqui :)
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#fff" },
+  imageWrapper: {
+    position: 'relative',
+    backgroundColor: "#fff",
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    elevation: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.15,
+    shadowRadius: 15,
+  },
+  image: { width: "100%", height: 350, borderBottomLeftRadius: 40, borderBottomRightRadius: 40 },
+  favoriteButtonContainer: { position: 'absolute', bottom: -25, right: 30, zIndex: 10 },
+  favoriteButton: {
+    backgroundColor: '#fff',
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  content: { padding: 20, paddingTop: 40 },
+  title: { fontSize: 28, fontWeight: "bold", color: "#573011", marginBottom: 5 },
+  subtitle: { fontSize: 20, fontWeight: "600", color: "#D4A373", marginTop: 20, marginBottom: 10 },
+  text: { fontSize: 16, lineHeight: 24, color: "#555" },
+});
